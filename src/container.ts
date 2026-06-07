831edbad5889165ec4b97de47d6373c6576d4cf9
Peron <peron_ap@icloud.com>
2026-06-07 22:23:08 +0800

    fix: wait for ports before starting

--- src/container.ts
+++ src/container.ts
@@ -3,11 +3,15 @@ import { Container } from "@cloudflare/containers";
 export class SshContainer extends Container {
 	override defaultPort = 2052;
 
-	override fetch(request: Request): Promise<Response> {
+	override async fetch(request: Request): Promise<Response> {
 		const authorizedKey = request.headers.get("X-Authorized-Key");
-		if (authorizedKey) {
-			this.envVars = { AUTHORIZED_KEY: authorizedKey };
-		}
+		await this.startAndWaitForPorts({
+			startOptions: {
+				envVars: {
+					AUTHORIZED_KEY: authorizedKey!,
+				},
+			},
+		});
 		return super.fetch(request);
 	}
 }
