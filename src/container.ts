a119ad5862303b36d691c913c621c4279c5dfd64
Peron <peron_ap@icloud.com>
2026-06-08 15:39:19 +0800

    feat: extend container sleepAfter to 24h

--- src/container.ts
+++ src/container.ts
@@ -2,6 +2,7 @@ import { Container } from "@cloudflare/containers";
 
 export class SshContainer extends Container {
 	override defaultPort = 2052;
+	override sleepAfter = "24h";
 
 	override async fetch(request: Request): Promise<Response> {
 		const authorizedKey = request.headers.get("X-Authorized-Key");
